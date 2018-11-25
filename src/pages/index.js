24d08cda6f496a8ad24b362c661468c6108d1094
Kevin Smith <ksmithbaylor@gmail.com>
2018-11-24 21:24:45 -0500

    Add emotion

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,15 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import styled from 'react-emotion'
 
 import Layout from '~/components/layout'
-import Image from '~/components/image'
+
+const Thing = styled.div`
+  color: blue;
+`
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Thing>blue</Thing>
   </Layout>
 )
 
